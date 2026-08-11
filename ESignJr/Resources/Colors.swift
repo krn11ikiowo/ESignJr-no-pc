a6cf47c1eed3f7aa64e36fe61fee9e4445de12f8
akkuenon <worleyjeremiah80@gmail.com>
2026-08-11 10:06:05 -0400

    Add AppColors utility file

---
+++ ESignJr/Resources/Colors.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+/// App color scheme
+struct AppColors {
+    static let primary = UIColor.systemBlue
+    static let secondary = UIColor.systemGray
+    static let accent = UIColor.systemOrange
+    static let background = UIColor.systemBackground
+    static let surfaceBackground = UIColor.secondarySystemBackground
+    static let text = UIColor.label
+    static let secondaryText = UIColor.secondaryLabel
+    static let error = UIColor.systemRed
+    static let success = UIColor.systemGreen
+}
