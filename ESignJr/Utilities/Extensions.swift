f019a69a8cb91547354617e7f37ecded40cbf523
akkuenon <worleyjeremiah80@gmail.com>
2026-08-11 10:06:00 -0400

    Add Extensions utility file

---
+++ ESignJr/Utilities/Extensions.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// MARK: - UIViewController Extensions
+extension UIViewController {
+    /// Show an error alert
+    func showErrorAlert(title: String = "Error", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
+    /// Show a success message
+    func showSuccessAlert(title: String = "Success", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - Date Extensions
+extension Date {
+    /// Format date to readable string
+    func formattedString() -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter.string(from: self)
+    }
+}
